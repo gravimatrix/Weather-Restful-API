27d6e964037445fe427ef3b0d9c73dfbc4ed71b9
sandeep <sandeepsahooiit@gmail.com>
2021-04-01 00:25:35 +0530

    Changed e to evt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const tweetForm = document.querySelector("#tweetForm");
-tweetForm.addEventListener("submit",function(e){
+tweetForm.addEventListener("submit",function(evt){
   const userNameInput = document.querySelectorAll("input")[0];
   const tweetInput = document.querySelectorAll("input")[1];
   console.log(userNameInput.value);
-  e.preventDefault();
+  evt.preventDefault();
 });
