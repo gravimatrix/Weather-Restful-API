f29546001a0fb586a98cb9a4da2cd58fe936db85
sandeep <sandeepsahooiit@gmail.com>
2021-04-02 00:39:49 +0530

    Deleted index.html,index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const tweetForm = document.querySelector("#tweetForm");
-tweetForm.addEventListener("submit",function(evt){
-  const userNameInput = document.querySelectorAll("input")[0];
-  const tweetInput = document.querySelectorAll("input")[1];
-  console.log(userNameInput.value);
-  evt.preventDefault();
-});
