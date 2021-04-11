4ecaeb3a32ba9ee1b39b8f0a6db97280137f4d2c
sandeep <sandeepsahooiit@gmail.com>
2021-04-11 13:21:41 +0530

    changed index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const delayedColorChange = (color,delay,doNext) => {
-    setTimeout(()=>{
-        document.body.style.backgroundColor = color;
-        doNext && doNext();
-    },delay);
+const delayedColorChange = (color, delay, doNext) => {
+  setTimeout(() => {
+    document.body.style.backgroundColor = color;
+    doNext && doNext();
+  }, delay);
 };
 
-delayedColorChange("red",2000,()=>{
-    delayedColorChange("green",2000,()=>{
-        delayedColorChange("blue",2000);
-    })
-})
+delayedColorChange("red", 2000, () => {
+  delayedColorChange("green", 2000, () => {
+    delayedColorChange("blue", 2000, () => {
+      delayedColorChange("violet", 2000, () => {
+        delayedColorChange("yellow", 2000, () => {
+          delayedColorChange("indigo", 2000, () => {
+            delayedColorChange("orange", 2000);
+          });
+        });
+      });
+    });
+  });
+});
