05b653d6cbb50a5de8c5494b59a47cc88ae8e1f2
sandeep <sandeepsahooiit@gmail.com>
2021-04-11 19:08:37 +0530

    Used async and await

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const delayedColorChange = (color, delay) => {
   return new Promise((resolve, reject) => {
+    var toChange = true;
+    if (color === "red") {
+      reject("I don't like fucking red");
+      toChange = false;
+    }
     setTimeout(() => {
-      document.body.style.backgroundColor = color;
+      if (toChange) document.body.style.backgroundColor = color;
       resolve();
     }, delay);
   });
 };
 
-delayedColorChange("red", 2000)
-  .then(() => delayedColorChange("blue", 2000))
-  .then(() => delayedColorChange("green", 2000))
-  .then(() => delayedColorChange("orange", 2000))
-  .then(() => delayedColorChange("violet", 2000))
-  .then(() => delayedColorChange("indigo", 2000))
-  .then(() => delayedColorChange("yellow", 2000))
+const rainbow = async () => {
+  try {
+    await delayedColorChange("violet", 2000);
+    await delayedColorChange("indigo", 2000);
+    await delayedColorChange("blue", 2000);
+    await delayedColorChange("green", 2000);
+    await delayedColorChange("red", 2000);
+    await delayedColorChange("yellow", 2000);
+    await delayedColorChange("orange", 2000);
+  } catch (error) {
+    return "I don't like fucking red, and won't change color to and after red";
+  }
+  return "Changing BG color to rainbow";
+};
+
+rainbow()
+  .then((msg) => console.log(msg))
+  .catch((err) => console.log(err));
