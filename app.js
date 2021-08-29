fd6bdf36b622e00910cfcb79dbbc02cddb614207
sandeep <sandeepsahooiit@gmail.com>
2021-08-29 15:02:57 +0530

    weatherstack API using axios package

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const request = require("request");
+const axios = require("axios");
 
-var url =
-  "http://api.weatherstack.com/current?" +
-  "access_key=3e8cfdead0dc17e93742f7fa9c9a26b8" +
-  "&query=Dhenkanal" +
-  "&units=m";
-
-request({ url: url, json: true }, (err, res) => {
-  if (err) {
-    console.log("Unable to connect to weatherstack: ",err);
-  } else if (res.body.error) {
-    console.log("Unable to get the response: ",res.body.error);
-  } else {
-    console.log(res.body.current);
-  }
-});
+axios
+  .get("http://api.weatherstack.com/current", {
+    params: {
+      access_key: "3e8cfdead0dc17e93742f7fa9c9a26b8",
+      query: "Dhenkanal",
+      units: "m",
+    },
+  })
+  .then(function (response) {
+    console.log(response.data.current);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
